88bebea8f87f0a0e6a4d54247c140bca366a175e
Lynn Kim <lynn.kim@brandung.de>
2023-07-05 11:42:54 +0200

    add webfont

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
+import React, { Component } from "react";
+import Deck from "./components/Deck";
 import "./App.css";
 
-function App() {
-  return (
-    <div className='App'>
-      <p>test</p>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Deck />
+      </div>
+    );
+  }
 }
-
-export default App;
